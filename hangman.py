6d5b7dd92b9b5c8a053ed2098909a249465e947a
AMALNR98 <amalnr123@gmail.com>
2022-08-03 17:11:23 +0530

    get_random_word function passed 4 test cases

--- hangman.py
+++ hangman.py
@@ -1,12 +1,13 @@
-import string
 import random
 
-def get_random_word():
-    file_for_random_word = open("/usr/share/dict/words","r").readlines()
+def get_random_word(path="/usr/share/dict/words"):
+    file_for_random_word = open(path, "r").readlines()
     while 1:
         random_word = random.choice(file_for_random_word).strip()
-        if len(random_word)>=6:
-            print(random_word)
-            break
+        if len(random_word)>=6 and random_word.isalpha() and not random_word[0].isupper():
+            # print(random_word)
+            return random_word
+            
+    
 
-get_random_word()
+print(get_random_word())
